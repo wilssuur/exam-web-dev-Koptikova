5e4113eef9d648706dbcd2f238d492664b992a71
wilssuur <koptikova_liliya@icloud.com>
2024-01-05 17:33:24 +0300

    completed pagination

--- exam/main.js
+++ exam/main.js
@@ -15,7 +15,6 @@ function createListItemElement(record) {
     buttonElement.classList.add('px-4', 'py-2', 'border',
         'border-2', 'rounded-2');
 
-
     let itemElementId = document.createElement('td');
     itemElementId.classList.add("d-none", "id-route");
 
@@ -29,6 +28,20 @@ function createListItemElement(record) {
     itemElementDesc.append(record.description);
     itemElementObj.append(record.mainObject);
     itemElementButton.append(buttonElement);
+
+    if (itemElementDesc.innerHTML.length > 150) {
+        itemElementDesc.setAttribute('data-bs-toggle', 'tooltip');
+        itemElementDesc.setAttribute('title', `${itemElementDesc.innerHTML}`);
+        itemElementDesc.innerHTML =
+            itemElementDesc.innerHTML.slice(0, 150) + '...';
+    }
+
+    if (itemElementObj.innerHTML.length > 150) {
+        itemElementObj.setAttribute('data-bs-toggle', 'tooltip');
+        itemElementObj.setAttribute('title', `${itemElementObj.innerHTML}`);
+        itemElementObj.innerHTML =
+            itemElementObj.innerHTML.slice(0, 150) + '...';
+    }
 
     itemElement.append(itemElementId, itemElementName, itemElementDesc,
         itemElementObj, itemElementButton);
@@ -46,7 +59,6 @@ function buttonStatesActive() {
     let pageButtons = document.querySelectorAll('.pagination li span');
     pageButtons = Array.from(pageButtons).splice(1, 5);
     pageButtons.forEach((button, index) => {
-        console.log(button.innerHTML, currentPage);
         if (button.innerHTML == currentPage) {
             button.classList.add('active');
         } else {
@@ -57,10 +69,17 @@ function buttonStatesActive() {
 
 function buttonStatesDisabled(page) {
     let buttonLast = document.getElementById('last');
+    let buttonNext = document.getElementById('next');
+
     if (page == 1) {
         buttonLast.classList.add('disabled');
+
+    } else if (page == 24) {
+        buttonNext.classList.add('disabled');
+
     } else {
         buttonLast.classList.remove('disabled');
+        buttonNext.classList.remove('disabled');
     }
 }
 
@@ -75,7 +94,15 @@ function lastGroupButtons() {
 function nextGroupButtons() {
     let pageButtons = document.querySelectorAll('.pagination li span');
     pageButtons = Array.from(pageButtons).splice(1, 5);
+
+    let items = Array.from(content.getElementsByTagName('tr'));
+    let startIndex = currentPage * itemsPerPage;
+    let countItems = items.slice(startIndex).length;
+
     pageButtons.forEach((button, index) => {
+        if ((index + 1) > Math.ceil(countItems / 5)) {
+            button.classList.add('d-none');
+        }
         button.innerHTML = Number(button.innerHTML) + 5;
     });
 }
@@ -93,18 +120,22 @@ function pageBtnHandler(event) {
     } else if (page == 'Предыдущая') {
         if ((currentPage - 1) % 5 == 0) {
             lastGroupButtons();
+
         }
         currentPage = currentPage - 1;
 
-    } else if (page) {
+    } else if (Number(page)) {
         currentPage = Number(page);
     }
+
     let startIndex = (currentPage - 1) * itemsPerPage;
     let endIndex = startIndex + itemsPerPage;
+
     items.forEach((item, index) => {
         item.classList.toggle('d-none', index < startIndex
             || index >= endIndex);
     });
+
     buttonStatesDisabled(currentPage);
     buttonStatesActive();
 
