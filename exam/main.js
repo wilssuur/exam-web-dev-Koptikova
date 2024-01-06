a3b162e7723caf53236cc7df95250ffa21197798
wilssuur <koptikova_liliya@icloud.com>
2024-01-07 02:28:21 +0300

    start search functions

--- exam/main.js
+++ exam/main.js
@@ -1,7 +1,8 @@
 let apiKey = 'api_key=b3cd4c4c-4324-454e-a437-942d700bd5ad';
 let hostRoutes = 'http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes';
 
-const content = document.querySelector('.content');
+let content = document.querySelector('#routes-list');
+let newRoutes = [];
 const itemsPerPage = 5;
 let currentPage = 1;
 let page = 1;
@@ -12,13 +13,15 @@ function createListItemElement(record) {
     let buttonElement = document.createElement('button');
     let textButton = "Выбрать";
     buttonElement.append(textButton);
-    buttonElement.classList.add('px-4', 'py-2', 'border',
-        'border-2', 'rounded-2');
+    buttonElement.classList.add('px-3', 'py-2', 'border',
+        'border-1', 'rounded-2');
 
     let itemElementId = document.createElement('td');
-    itemElementId.classList.add("d-none", "id-route");
+    itemElementId.classList.add("d-none", "route-id");
 
     let itemElementName = document.createElement('td');
+    itemElementName.classList.add("route-name",);
+
     let itemElementDesc = document.createElement('td');
     let itemElementObj = document.createElement('td');
     let itemElementButton = document.createElement('td');
@@ -108,37 +111,105 @@ function nextGroupButtons() {
 }
 
 function pageBtnHandler(event) {
-    let items = Array.from(content.getElementsByTagName('tr'));
-    let page = event.target.innerHTML;
+    if (document.querySelector('#search-field').value == '') {
+        let items = Array.from(content.getElementsByTagName('tr'));
+        console.log(items);
+        let page = event.target.innerHTML;
 
-    if (page == 'Следующая') {
-        if (currentPage % 5 == 0) {
-            nextGroupButtons();
+        if (page == 'Следующая') {
+            if (currentPage % 5 == 0) {
+                nextGroupButtons();
+            }
+            currentPage = currentPage + 1;
+
+        } else if (page == 'Предыдущая') {
+            if ((currentPage - 1) % 5 == 0) {
+                lastGroupButtons();
+
+            }
+            currentPage = currentPage - 1;
+
+        } else if (Number(page)) {
+            currentPage = Number(page);
         }
-        currentPage = currentPage + 1;
 
-    } else if (page == 'Предыдущая') {
-        if ((currentPage - 1) % 5 == 0) {
-            lastGroupButtons();
+        let startIndex = (currentPage - 1) * itemsPerPage;
+        let endIndex = startIndex + itemsPerPage;
 
-        }
-        currentPage = currentPage - 1;
+        items.forEach((item, index) => {
+            item.classList.toggle('d-none', index < startIndex
+                || index >= endIndex);
+        });
 
-    } else if (Number(page)) {
-        currentPage = Number(page);
+        buttonStatesDisabled(currentPage);
+        buttonStatesActive();
+        
+    } else {
+        console.log(newRoutes);
     }
+}   
 
-    let startIndex = (currentPage - 1) * itemsPerPage;
+function showItems(items) {
+    newRoutes = [];
+    items.forEach((item) => {
+        if (item.className != 'd-none') {
+            newRoutes.push(item);
+        }
+    });
+
+    let startIndex = 0;
     let endIndex = startIndex + itemsPerPage;
-
-    items.forEach((item, index) => {
+    newRoutes.forEach((item, index) => {
         item.classList.toggle('d-none', index < startIndex
             || index >= endIndex);
     });
 
-    buttonStatesDisabled(currentPage);
-    buttonStatesActive();
+    let pageButtons = document.querySelectorAll('.pagination li span');
+    pageButtons = Array.from(pageButtons).splice(1, 5);
 
+    let countItems = newRoutes.length;
+    pageButtons.forEach((button, index) => {
+        if ((index + 1) > Math.ceil(countItems / 5)) {
+            button.classList.add('d-none');
+        } else {
+            button.classList.remove('d-none');
+        }
+    });
+}
+
+function searchHandler() {
+    let searchText = document.querySelector('#search-field').value;
+    let items = Array.from(content.getElementsByTagName('tr'));
+
+    if (searchText == '') {
+        console.log(items);
+        let pageButtons = document.querySelectorAll('.pagination li span');
+        pageButtons = Array.from(pageButtons).splice(1, 5);
+        pageButtons.forEach((button) => {
+            button.classList.remove('d-none');
+        });
+
+        let startIndex = 0;
+        let endIndex = startIndex + itemsPerPage;
+        items.forEach((item, index) => {
+            item.classList.toggle('d-none', index < startIndex
+                || index >= endIndex);
+        });
+        let buttonLast = document.getElementById('last');
+        buttonLast.classList.add('disabled');
+
+    } else {
+        items.forEach((item) => {
+            let itemName =
+                item.querySelector('.route-name').innerHTML.toLowerCase();
+            if (!(itemName.includes(searchText.toLowerCase()))) {
+                item.classList.add('d-none');
+            } else {
+                item.classList.remove('d-none');
+            }
+        });
+        showItems(items);
+    }
 }
 
 async function LoadStorage() {
@@ -160,8 +231,8 @@ async function LoadStorage() {
     buttonLast.classList.add('disabled');
 }
 
-
 window.onload = function () {
     LoadStorage();
     document.querySelector('.pagination').onclick = pageBtnHandler;
+    document.querySelector('#search-field').oninput = searchHandler;
 };
